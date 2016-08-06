d44a4a53e96a1cb780ca18d1b1fa1b60b7a0e029
Alen Thomas <alenthomas9@gmail.com>
2016-08-06 10:22:49 +0530

    adds a serialzier for GeoLocationField, also create a new theatre serializer with GeofiledSr

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import Movie, Show, Time
+from .models import Movie, Show, Time, Theatre
 
+
+class LocationField(serializers.Field):
+
+    def to_representation(self, obj):
+        location = {'lat': obj.latitude, 'long': obj.longitude}
+        return location
+
+class TheatreSr(ModelSerializer):
+    location = LocationField()
+    class Meta:
+        model = Theatre
+        fileds = ('location',)
 
 class TimeSr(ModelSerializer):
 
@@ -18,14 +30,14 @@ class ShowSr(ModelSerializer):
 
 
 class MovieSr(ModelSerializer):
-
     class Meta:
         model = Movie
         fields = '__all__'
 
 class ShowDetailSr(ModelSerializer):
     time = TimeSr(many=True)
+    theatre = TheatreSr()
     class Meta:
         model = Show
-        fields = ('movie', 'time', 'hall',)
+        fields = ('movie', 'time', 'hall', 'theatre',)
         depth = 2
