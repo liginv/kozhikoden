7fca0a8310c07953b47095f753d8849b7740630f
Alen Thomas <alenthomas9@gmail.com>
2016-08-06 10:23:26 +0530

    leverages GeoLocationSerializer in culturals.serialziers and restaurent.serializers

--- culturals/serializers.py
+++ culturals/serializers.py
@@ -1,8 +1,8 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import Cultural
-
+from movies.serializers import LocationField
 
 class CulturalSr(ModelSerializer):
 
@@ -11,7 +11,8 @@ class CulturalSr(ModelSerializer):
         fields = ('id', 'name', 'pic', 'event_type',)
 
 class CulturalDetailSr(ModelSerializer):
-
+    location = LocationField()
+    
     class Meta:
         model = Cultural
         fields = '__all__'
--- restaurents/serializers.py
+++ restaurents/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Restaurent
+from movies.serializers import LocationField
 
 
 class RestaurentSr(ModelSerializer):
@@ -11,7 +12,7 @@ class RestaurentSr(ModelSerializer):
         fields = ('id', 'name', 'pic', 'ambience',)
 
 class RestaurentDetailSr(ModelSerializer):
-
+    location = LocationField()
     class Meta:
         model = Restaurent
         fields = '__all__'
