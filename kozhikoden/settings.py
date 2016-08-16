5681afea9854e20058d6d6020833a5cb814fde6f
Ligin Vellakkad <ligindd@gmail.com>
2016-08-16 16:07:49 +0530

    Made static & media folder accessable in any system.

--- kozhikoden/settings.py
+++ kozhikoden/settings.py
@@ -2,6 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -133,9 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '*/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 MEDIA_URL = '/media/'
 
 # SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
