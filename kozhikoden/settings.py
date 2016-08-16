c7aea365b1547e2f8979e47c942756a70e0d13f7
Ligin Vellakkad <ligindd@gmail.com>
2016-08-16 16:27:30 +0530

    link the path of static files through BASE_DIR

--- kozhikoden/settings.py
+++ kozhikoden/settings.py
@@ -2,7 +2,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -136,13 +135,12 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, 'static'),
+                    )
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-                    os.path.join(PROJECT_ROOT, 'static'),
-                    )
 # SPATIALITE_LIBRARY_PATH = '/usr/local/lib/mod_spatialite.dylib'
 
 GEOPOSITION_GOOGLE_MAPS_API_KEY = ' AIzaSyDxd538__HtXgMiIpTohSMaYMxE-S4XWFw '
