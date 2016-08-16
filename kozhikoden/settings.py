033787e5af335b431d416d3a8d01e311501eafaa
Ligin Vellakkad <ligindd@gmail.com>
2016-08-16 17:22:23 +0530

    Updated to use static files using whitenoise.

--- kozhikoden/settings.py
+++ kozhikoden/settings.py
@@ -149,3 +149,5 @@ GEOPOSITION_GOOGLE_MAPS_API_KEY = ' AIzaSyDxd538__HtXgMiIpTohSMaYMxE-S4XWFw '
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
