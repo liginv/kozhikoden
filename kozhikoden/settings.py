259cf52bc5da8f5b491592627886f54f3db5f164
Ligin Vellakkad <ligindd@gmail.com>
2016-08-17 07:38:48 +0530

    Added postgresql db to the settings.py

--- kozhikoden/settings.py
+++ kozhikoden/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbkozhikoden',
+        'USER': 'dbuser',
+        'PASSWORD': 'dbpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
